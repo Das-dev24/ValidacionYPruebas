9701c176bb53c05ab478b408772f36cb8460ccfa
Diego <das1010@alu.ubu.es>
2025-10-22 17:30:38 +0200

    Corregido Tests 1 y funciona

--- Practica/WWWTest/AdminLogIn.cs
+++ Practica/WWWTest/AdminLogIn.cs
@@ -1,11 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
 {
@@ -16,14 +17,14 @@ namespace SeleniumTests
         private StringBuilder verificationErrors;
         private static string baseURL;
         private bool acceptNextAlert = true;
-        
+
         [ClassInitialize]
         public static void InitializeClass(TestContext testContext)
         {
-            driver = new FirefoxDriver();
+            driver = new ChromeDriver();
             baseURL = "https://www.google.com/";
         }
-        
+
         [ClassCleanup]
         public static void CleanupClass()
         {
@@ -37,19 +38,19 @@ namespace SeleniumTests
                 // Ignore errors if unable to close the browser
             }
         }
-        
+
         [TestInitialize]
         public void InitializeTest()
         {
             verificationErrors = new StringBuilder();
         }
-        
+
         [TestCleanup]
         public void CleanupTest()
         {
             Assert.AreEqual("", verificationErrors.ToString());
         }
-        
+
         [TestMethod]
         public void TheAdminLogInTest()
         {
@@ -60,47 +61,7 @@ namespace SeleniumTests
             driver.FindElement(By.Id("txtPassword")).Clear();
             driver.FindElement(By.Id("txtPassword")).SendKeys("Admin123456!");
             driver.FindElement(By.Id("btnLogin")).Click();
-            driver.Navigate().GoToUrl("https://localhost:44396/MainView.aspx");
-        }
-        private bool IsElementPresent(By by)
-        {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
-        }
-        
-        private bool IsAlertPresent()
-        {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
-        }
-        
-        private string CloseAlertAndGetItsText() {
-            try {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert) {
-                    alert.Accept();
-                } else {
-                    alert.Dismiss();
-                }
-                return alertText;
-            } finally {
-                acceptNextAlert = true;
-            }
+            Assert.AreEqual("https://localhost:44396/MainView.aspx", driver.Url);
         }
     }
 }
