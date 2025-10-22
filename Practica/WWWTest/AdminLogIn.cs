266cedddd5bccb7384bce26bd94f31e1bf99b5ec
Diego <das1010@alu.ubu.es>
2025-10-22 20:45:15 +0200

    Tests de logIn automáticos

--- Practica/WWWTest/AdminLogIn.cs
+++ Practica/WWWTest/AdminLogIn.cs
@@ -1,3 +1,4 @@
+using Microsoft.CodeCoverage.Core.Reports.Coverage;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -13,8 +14,23 @@ namespace SeleniumTests
     [TestClass]
     public class AdminLogIn
     {
-        [TestMethod]
-        public void TheAdminLogInTest()
+        public static IEnumerable<object[]> ReadCSV()
+        {
+            string FilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestData", "LogIn.csv");
+
+            return File.ReadLines(FilePath)
+                .Skip(1) // Saltar la línea de encabezado
+                .Select(line => line.Split(','))
+                .Select(values => new object[]
+                {
+                    values[0].Trim(),
+                    values[1].Trim(),
+                    int.Parse(values[2].Trim())
+                });
+        }
+        [DataTestMethod]
+        [DynamicData(nameof(ReadCSV), DynamicDataSourceType.Method)]
+        public void TheAdminLogInTest(string email, string password, int is_succes)
         {
             IWebDriver driver = new ChromeDriver();
             string baseURL = "https://www.google.com/";
@@ -22,11 +38,14 @@ namespace SeleniumTests
             driver.Navigate().GoToUrl("https://localhost:44396/LogIn.aspx");
             driver.FindElement(By.Id("txtEmail")).Click();
             driver.FindElement(By.Id("txtEmail")).Clear();
-            driver.FindElement(By.Id("txtEmail")).SendKeys("admin@example.com");
+            driver.FindElement(By.Id("txtEmail")).SendKeys(email);
             driver.FindElement(By.Id("txtPassword")).Clear();
-            driver.FindElement(By.Id("txtPassword")).SendKeys("Admin123456!");
+            driver.FindElement(By.Id("txtPassword")).SendKeys(password);
             driver.FindElement(By.Id("btnLogin")).Click();
-            Assert.AreEqual("https://localhost:44396/MainView.aspx", driver.Url);
+            if (is_succes == 0)
+                Assert.AreEqual("https://localhost:44396/LogIn.aspx", driver.Url);
+            else
+                Assert.AreEqual("https://localhost:44396/MainView.aspx", driver.Url);
 
             try
             {
